58460d70edc37f27bb44edc1116e9391138fb92f
Hunain Ali Khan <hunainalikhan4@gmail.com>
2020-10-08 01:50:38 +0500

    new commit

--- encore-sdk-support/src/main/java/com/encorepay/encore_sdk_support/OuterToastMessage.java
+++ encore-sdk-support/src/main/java/com/encorepay/encore_sdk_support/OuterToastMessage.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.encorepay.card_scanner.Commons;
 
+
 public class OuterToastMessage {
     public static String giveMessage(Context context, String text){
        return Commons.toastMessage(context,text,text);
