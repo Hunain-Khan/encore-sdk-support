62630522304844daccd0bcef97cb6b97b99b51aa
Hunain Ali Khan <hunainalikhan4@gmail.com>
2020-10-07 13:50:28 +0500

    new function added

--- encore-sdk-support/src/main/java/com/encorepay/encore_sdk_support/NextActivity.java
+++ encore-sdk-support/src/main/java/com/encorepay/encore_sdk_support/NextActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.encorepay.card_scanner.CardScannerActivity;
 
@@ -24,5 +25,10 @@ public class NextActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    public void toastMessage(){
+        Toast.makeText(this,"this message is from dependent sdk",Toast.LENGTH_SHORT).show();
     }
 }
