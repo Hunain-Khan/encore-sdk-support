65e89105ad7dbf651780f585498833e3c12bedd6
Hunain Ali Khan <hunainalikhan4@gmail.com>
2020-10-16 13:52:58 +0500

    add cardScanProvider

---
+++ cardscan/src/main/java/com/encorepay/cardscan/CardScanProvider.java
@@ -0,0 +1,27 @@
+package com.encorepay.cardscan;
+import android.app.Activity;
+
+import com.encorepay.card_scanner.ScanDocument;
+import com.encorepay.card_scanner.ScanSide;
+
+public class CardScanProvider {
+   private ScanDocument scanDocument;
+   public static final String CARD_FRONT_IMAGE = "cardFrontSideImage";
+   public static final String CARD_BACK_IMAGE = "cardBackSideImage";
+
+    public CardScanProvider(){
+        scanDocument = new ScanDocument();
+    }
+
+    public void setCardScanSide(ScanSide scanSide){
+        scanDocument.setCardScanSide(scanSide);
+    }
+
+    public void startScanDocument(Activity activity, int REQ_CODE){
+        scanDocument.startScanActivity(activity,REQ_CODE);
+    }
+
+    public void flashLightEnabled(boolean enable){
+        scanDocument.flashLightEnabled(enable);
+    }
+}
